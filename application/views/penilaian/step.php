2e683282f8cf5a7c7f7fc967820a6a19528a5584
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2022-06-13 19:14:52 +0700

    fixing bugs

--- application/views/penilaian/step.php
+++ application/views/penilaian/step.php
@@ -12,16 +12,16 @@
 					$dataKategori = $this->db->query('SELECT * FROM kategori  ORDER BY kategori_id ASC') ?>
 
 					<?php foreach ($dataKategori->result() as $row) { ?>
-						<?php if($kategori_id == $row->kategori_id ){ ?>
-							<a href="<?= base_url() ?>penilaian/step?p=<?=$priode ?>&k=<?=$row->kategori_id ?>" class="btn btn-warning" style="margin-bottom: 5px;">
-							Kategori <?= $row->kode_kategori ?>
-						</a>
-						<?php }else{ ?>
-							<a href="<?= base_url() ?>penilaian/step?p=<?=$priode ?>&k=<?=$row->kategori_id ?>" class="btn btn-success" style="margin-bottom: 5px;">
-							Kategori <?= $row->kode_kategori ?>
-						</a>
+						<?php if ($kategori_id == $row->kategori_id) { ?>
+							<a href="<?= base_url() ?>penilaian/step?p=<?= $priode ?>&k=<?= $row->kategori_id ?>" class="btn btn-warning" style="margin-bottom: 5px;">
+								Kategori <?= $row->kode_kategori ?>
+							</a>
+						<?php } else { ?>
+							<a href="<?= base_url() ?>penilaian/step?p=<?= $priode ?>&k=<?= $row->kategori_id ?>" class="btn btn-success" style="margin-bottom: 5px;">
+								Kategori <?= $row->kode_kategori ?>
+							</a>
 						<?php } ?>
-						
+
 					<?php } ?>
 				</div>
 			</div>
@@ -35,68 +35,51 @@
 					<h4 class="panel-title">Penilaian Kategori <?= $query->kode_kategori ?> : <?= $query->nama_kategori ?> ( Bobot Maks : <?= $query->bobot ?> Point) </h4>
 				</div>
 				<div class="panel-body">
+					<form class=""action="<?= base_url() ?>penilaian/step" method="GET">
+						<table id="" class="table table-bordered table-hover table-td-valign-middle">
+							<thead>
+								<tr>
+									<th>#</th>
+									<th>Nama Pegawai</th>
+									<th>Nilai</th>
+								</tr>
+							</thead>
+							<?php
+							$no = 1;
+							$pegawai = $this->db->query("SELECT * from karyawan where jabatan_id='2'");
+							?>
+							<?php foreach ($pegawai->result() as $row) { ?>
+								<tr>
+									<td><?= $no++ ?></td>
+									<td><?= $row->nama_karyawan  ?></td>
+									<td><input type="number" required max="<?= $query->bobot ?>" min="0" class="form-control" name="nilai[]" id="nialai" placeholder="Nilai" value="<?= getNilai($row->karyawan_id, $kategori_id, $priode) ?>" /></td>
+								</tr>
+							<?php } ?>
+						</table>
 
-
-					<table id="" class="table table-bordered table-hover table-td-valign-middle">
-						<thead>
-							<tr>
-								<th>#</th>
-								<th>Nama Pegawai</th>
-								<th>Nilai</th>
-							</tr>
-						</thead>
-						<?php
-						$no = 1;
-						$pegawai = $this->db->query("SELECT * from karyawan where jabatan_id='2'");
+						<!-- cek last ID -->
+						<?php $cek = $this->db->query('SELECT * FROM kategori  ORDER BY kategori_id DESC LIMIT 1')->row();
+						$lastID = $cek->kategori_id;
 						?>
-						<?php foreach ($pegawai->result() as $row) { ?>
-							<tr>
-								<td><?= $no++ ?></td>
-								<td><?= $row->nama_karyawan  ?></td>
-								<td><input type="text" class="form-control" name="nik" id="nik" placeholder="Nilai" value="<?= getNilai($row->karyawan_id, $kategori_id, $priode) ?>" /></td>
-							</tr>
-						<?php } ?>
-					</table>
 
-					<!-- cek last ID -->
-					<?php $cek = $this->db->query('SELECT * FROM kategori  ORDER BY kategori_id DESC LIMIT 1')->row();
-					$lastID = $cek->kategori_id;
-					?>
-
-					<?php if ($kategori_id == $lastID) { ?>
-						<!-- <a href='javascript:history.back()'>
-							<button class="btn btn-default" style="float: left;">
-								<i class="fa fa-arrow-left"></i>
-								Kembali
-							</button>
-						</a> -->
-						<form class="" action="" method="GET">
-							<input type="hidden" required name="b" value="<?= $kategori_id ?>">
-							<button class="btn btn-danger" style="float: right;">
+						<?php if ($kategori_id == $lastID) { ?>
+							<input type="hidden" required name="p" value="<?= $priode ?>">
+							<input type="hidden" required name="k" value="<?= $kategori_id + 1 ?>">
+							<button class="btn btn-danger" type="submit" style="float: right;">
 								<i class="fa fa-arrow-right"></i>
 								Finish
 							</button>
-						</form>
+					</form>
+				<?php } else { ?>
 
-					<?php } else { ?>
-						<!-- <a href='javascript:history.back()'>
-							<button class="btn btn-default" style="float: left;">
-								<i class="fa fa-arrow-left"></i>
-								Kembali
-							</button>
-						</a> -->
-
-						<form class="" action="<?= base_url() ?>penilaian/step" method="GET">
-							<!-- priode -->
-							<input type="hidden" required name="p" value="<?= $priode ?>">
-							<!-- kategori_id -->
-							<input type="hidden" required name="k" value="<?= $kategori_id +1 ?>">
-							<button class="btn btn-primary" style="float: right;">
-								<i class="fa fa-save"></i>
-								Simpan
-							</button>
-						</form>
-					<?php } ?>
+					<input type="hidden" required name="p" value="<?= $priode ?>">
+					<input type="hidden" required name="k" value="<?= $kategori_id + 1 ?>">
+					<button class="btn btn-primary" type="submit" style="float: right;">
+						<i class="fa fa-save"></i>
+						Simpan
+					</button>
+					</form>
+				<?php } ?>
 				</div>
 			</div>
 		</div>
