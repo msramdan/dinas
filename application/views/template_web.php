e939680db39764d928fe30f63281540925675250
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2022-11-14 15:09:10 +0700

    udpate

--- application/views/template_web.php
+++ application/views/template_web.php
@@ -522,6 +522,20 @@
 
         });
         </script>
+
+        <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            new Splide('.splide', {
+                type: 'loop',
+                drag: 'free',
+                focus: 'center',
+                perPage: 3,
+                autoScroll: {
+                    speed: 1,
+                },
+            }).mount(window.splide.Extensions);
+        });
+        </script>
 </body>
 
 
